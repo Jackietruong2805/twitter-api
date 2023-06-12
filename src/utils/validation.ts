acb24a1f618b4f89e3e7c1e1c2b5d5247f605c63
Jackietruong2805 <truonghao2805@gmail.com>
2023-06-12 14:32:42 +0700

    chuẩn hóa bộ xử lí lỗi

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import { body, validationResult, ValidationChain } from 'express-validator'
 import { RunnableValidationChains } from 'express-validator/src/middlewares/schema'
+import HTTP_STATUS from '~/constants/httpStatus'
+import { EntityError, ErrorWithStatus } from '~/models/Errors'
 // can be reused by many routes
 
 // sequential processing, stops running validations chain if the previous one fails.
@@ -11,7 +13,16 @@ export const validate = (validation: RunnableValidationChains<ValidationChain>) 
     if (errors.isEmpty()) {
       return next()
     }
+    const errorsObject = errors.mapped()
 
-    res.status(400).json({ errors: errors.mapped() })
+    const entityError = new EntityError({ errors: {} })
+    for (const key in errorsObject) {
+      const { msg } = errorsObject[key]
+      if (msg instanceof ErrorWithStatus && msg.status !== HTTP_STATUS.UNPROCESSABLE_ENTITY) {
+        return next(msg)
+      }
+      entityError.errors[key] = errorsObject[key]
+    }
+    next(entityError)
   }
 }
