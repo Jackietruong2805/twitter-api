227eca9d9e38d19ba893a5e937bb92568930c71b
Jackietruong2805 <truonghao2805@gmail.com>
2023-09-24 16:44:15 +0700

    Serving static file

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import databaseService from '~/services/database.services'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
 import mediasRouter from './routes/medias.routes'
 import { initFolder } from './utils/file'
+import { UPLOAD_DIR } from './constants/dir'
 databaseService.connect()
 const app = express()
 const port = process.env.PORT || 4000
@@ -14,6 +15,8 @@ initFolder()
 app.use(express.json())
 app.use('/users', usersRouter)
 app.use('/medias', mediasRouter)
+
+app.use('/static', express.static(UPLOAD_DIR))
 
 app.use(defaultErrorHandler)
 app.listen(port, () => {
