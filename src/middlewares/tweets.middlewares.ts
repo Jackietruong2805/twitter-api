bac0929b63c23d544dedca781ca915909715a435
Jackietruong2805 <truonghao2805@gmail.com>
2023-10-23 16:01:36 +0700

    Get comments, retweet, quotes with pagination

--- src/middlewares/tweets.middlewares.ts
+++ src/middlewares/tweets.middlewares.ts
@@ -129,7 +129,7 @@ export const tweetIdValidator = validate(
               .aggregate<Tweet>([
                 {
                   $match: {
-                    _id: new ObjectId('65313fa6244393d9253575db')
+                    _id: new ObjectId(value)
                   }
                 },
                 {
@@ -201,7 +201,7 @@ export const tweetIdValidator = validate(
                           input: '$tweet_children',
                           as: 'item',
                           cond: {
-                            $eq: ['$$item.type', 1]
+                            $eq: ['$$item.type', TweetType.Retweet]
                           }
                         }
                       }
@@ -212,7 +212,7 @@ export const tweetIdValidator = validate(
                           input: '$tweet_children',
                           as: 'item',
                           cond: {
-                            $eq: ['$$item.type', 2]
+                            $eq: ['$$item.type', TweetType.Comment]
                           }
                         }
                       }
@@ -223,7 +223,7 @@ export const tweetIdValidator = validate(
                           input: '$tweet_children',
                           as: 'item',
                           cond: {
-                            $eq: ['$$item.type', 3]
+                            $eq: ['$$item.type', TweetType.QuoteTweet]
                           }
                         }
                       }
