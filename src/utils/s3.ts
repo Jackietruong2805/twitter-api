87957dfcb96f11583519045b8f394c57aa89f7b5
Jackietruong2805 <truonghao2805@gmail.com>
2023-11-04 12:01:58 +0700

    Upload file to S3

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,5 +1,8 @@
+import { Upload } from '@aws-sdk/lib-storage'
 import { S3 } from '@aws-sdk/client-s3'
 import { config } from 'dotenv'
+import fs from 'fs'
+import path from 'path'
 config()
 const s3 = new S3({
   region: process.env.AWS_REGION,
@@ -8,5 +11,25 @@ const s3 = new S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID as string
   }
 })
+// D:\nodejs\Duthanhduoc\Twitter\uploads\images\temp\anh1.jpg
+const file = fs.readFileSync(path.resolve('uploads/images/temp/anh1.jpg'))
 
-s3.listBuckets({}).then((data) => console.log(data))
+const parallelUploads3 = new Upload({
+  client: s3,
+  params: { Bucket: 'twitter-project-clone', Key: 'anh3.jpg', Body: file, ContentType: 'image/jpeg' },
+
+  tags: [
+    /*...*/
+  ], // optional tags
+  queueSize: 4, // optional concurrency configuration
+  partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
+  leavePartsOnError: false // optional manually handle dropped parts
+})
+
+parallelUploads3.on('httpUploadProgress', (progress) => {
+  console.log(progress)
+})
+
+parallelUploads3.done().then((res) => {
+  console.log('res', res)
+})
