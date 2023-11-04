b6a8cbab8765efd5e5744e4c672384f90251452a
Jackietruong2805 <truonghao2805@gmail.com>
2023-11-04 15:54:13 +0700

    Update route upload image to S3

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -14,22 +14,31 @@ const s3 = new S3({
 // D:\nodejs\Duthanhduoc\Twitter\uploads\images\temp\anh1.jpg
 const file = fs.readFileSync(path.resolve('uploads/images/temp/anh1.jpg'))
 
-const parallelUploads3 = new Upload({
-  client: s3,
-  params: { Bucket: 'twitter-project-clone', Key: 'anh3.jpg', Body: file, ContentType: 'image/jpeg' },
+export const uploadFileToS3 = ({
+  filename,
+  filepath,
+  contentType
+}: {
+  filename: string
+  filepath: string
+  contentType: string
+}) => {
+  const parallelUploads3 = new Upload({
+    client: s3,
+    params: {
+      Bucket: 'twitter-project-clone',
+      Key: filename,
+      Body: fs.readFileSync(filepath),
+      ContentType: contentType
+    },
 
-  tags: [
-    /*...*/
-  ], // optional tags
-  queueSize: 4, // optional concurrency configuration
-  partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
-  leavePartsOnError: false // optional manually handle dropped parts
-})
+    tags: [
+      /*...*/
+    ], // optional tags
+    queueSize: 4, // optional concurrency configuration
+    partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
+    leavePartsOnError: false // optional manually handle dropped parts
+  })
 
-parallelUploads3.on('httpUploadProgress', (progress) => {
-  console.log(progress)
-})
-
-parallelUploads3.done().then((res) => {
-  console.log('res', res)
-})
+  return parallelUploads3.done()
+}
